e28ebb6ff4f2e490b3982196d49c163fbc26fc8b
Karl Sundar <anjukarl@gmail.com>
2022-09-08 22:40:01 +0530

    about & contact template added

--- src/app/shared/subhead/subhead.component.ts
+++ src/app/shared/subhead/subhead.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SubheadComponent implements OnInit {
   @Input() title = '';
+  @Input() heading = '';
 
   constructor() {}
 
